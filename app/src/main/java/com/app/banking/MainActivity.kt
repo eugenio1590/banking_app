92157514dbce94037a462f8f80b9720ae4e849b0
Eugenio Caicedo <eugeniohernandez17@gmail.com>
2024-03-07 17:15:05 -0500

    Activity updated

--- app/src/main/java/com/app/banking/MainActivity.kt
+++ app/src/main/java/com/app/banking/MainActivity.kt
@@ -6,13 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.app.banking.ui.screen.AccountsScreen
 import com.app.banking.ui.theme.BankingAppTheme
+import com.app.domain.model.User
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    // This object would be part of the user session
+    private val user = User(id = 1, name = "John Doe")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +26,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AccountsScreen(user = user)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BankingAppTheme {
-        Greeting("Android")
     }
 }
